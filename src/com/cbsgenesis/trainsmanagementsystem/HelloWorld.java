68fca009d32bd1e5ce47b988178dadcf8f90fcba
stinkymark1 <proselytear@yahoo.com>
2016-11-17 18:49:03 +0200

    Added class HelloWorld

---
+++ src/com/cbsgenesis/trainsmanagementsystem/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.cbsgenesis.trainsmanagementsystem;
+
+/**
+ * Simple class that prints message 'Hello world'
+ *
+ * @author Eugene Suleimanov <proselytear@yahoo.com>
+ * @version 0.1
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!!!");
+    }
+}
