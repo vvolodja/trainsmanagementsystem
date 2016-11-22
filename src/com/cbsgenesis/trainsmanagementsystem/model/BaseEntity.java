7c182e1928673f990d9a70a38ac213a96be9c867
stinkymark1 <proselytear@yahoo.com>
2016-11-22 19:30:44 +0200

    Added classe BaseEntity and NamedEntity

---
+++ src/com/cbsgenesis/trainsmanagementsystem/model/BaseEntity.java
@@ -0,0 +1,27 @@
+package com.cbsgenesis.trainsmanagementsystem.model;
+
+/**
+ * Base class that contains property 'id'.
+ * Used as a base class for all objects that need this property.
+ *
+ * @author Eugene Suleimanov
+ */
+
+public class BaseEntity {
+    private Long id;
+
+    public BaseEntity() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public boolean isNew(){
+        return (this.id == null);
+    }
+}
---
+++ src/com/cbsgenesis/trainsmanagementsystem/model/NamedEntity.java
@@ -0,0 +1,30 @@
+package com.cbsgenesis.trainsmanagementsystem.model;
+
+/**
+ * Class that extends class {@link BaseEntity} adding property 'name'.
+ * Used as a base class for all objects that need this property.
+ *
+ * @author Eugene Suleimanov
+ */
+
+public class NamedEntity extends BaseEntity {
+    private String name;
+
+    public NamedEntity() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "NamedEntity{" +
+                "Id: " + super.getId() +
+                "Name: " + name;
+    }
+}
