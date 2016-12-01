26dd6411ef54e221bd4eb192ff3d87527696b89b
stinkymark1 <proselytear@yahoo.com>
2016-12-01 18:54:33 +0200

    Added two types of wagoons

---
+++ src/main/java/com/cbsgenesis/trainsmanagementsystem/model/PassengerWaggon.java
@@ -0,0 +1,13 @@
+package com.cbsgenesis.trainsmanagementsystem.model;
+
+/**
+ * Simple JavaBean domain object that represents a Passenger Wagoon.
+ *
+ * @author Eugene Suleimanov
+ */
+
+public class PassengerWaggon extends Wagoon{
+    public PassengerWaggon(int weight) {
+        super(weight);
+    }
+}
